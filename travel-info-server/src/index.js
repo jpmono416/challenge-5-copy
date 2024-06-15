53f710a7b86e749ff526d356b00abf77f3a8b857
jpmono416 <jpmono416@gmail.com>
2024-06-15 15:32:31 +0100

    [Chore]: Created Server file

--- travel-info-server/src/index.js
+++ travel-info-server/src/index.js
@@ -1,8 +1,12 @@
 import Config from "./config/Config.js";
 import Database from "./db/Database.js";
+import Server from "./server/Server.js";
 
 Config.load();
 const { PORT, HOST, DB_URI } = process.env;
+
+// Add Routes
+//const server = new Server(PORT, HOST);
 const database = new Database(DB_URI);
 
 await database.connect();
