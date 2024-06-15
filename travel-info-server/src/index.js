326320e0d8738de5d0ca63b0bb6181797badea51
jpmono416 <jpmono416@gmail.com>
2024-06-15 16:39:33 +0100

    [Feat]: Server listening and DB connected - testing needed.

--- travel-info-server/src/index.js
+++ travel-info-server/src/index.js
@@ -1,12 +1,14 @@
 import Config from "./config/Config.js";
 import Database from "./db/Database.js";
 import Server from "./server/Server.js";
+import UserRoutes from "./routes/User.routes.js";
 
 Config.load();
 const { PORT, HOST, DB_URI } = process.env;
 
 // Add Routes
-//const server = new Server(PORT, HOST);
+const server = new Server(PORT, HOST, new UserRoutes());
 const database = new Database(DB_URI);
 
+server.start();
 await database.connect();
