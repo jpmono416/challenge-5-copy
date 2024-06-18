0850bb3f5c9a6ef9eec4dba7d59ea0e05cd45bda
jpmono416 <jpmono416@gmail.com>
2024-06-18 23:32:11 +0100

    [Feat]: Changed to /forecast API call and implemented some filtering on the response

--- travel-info-server/src/controllers/WeatherApi.controller.js
+++ travel-info-server/src/controllers/WeatherApi.controller.js
@@ -3,9 +3,74 @@ import WeatherApiService from "../services/WeatherApi.service.js";
 export default class WeatherApiController {
     static async getWeather(req, res) {
         try {
+            const hardcodedResponse = `{
+    "data": [
+    {
+        "date": "2024-06-18",
+        "avgTemp": 17.89,
+        "description": "light rain",
+        "avgHumidity": 64,
+        "avgPressure": 1016
+    },
+    {
+        "date": "2024-06-19",
+        "avgTemp": 16.05375,
+        "description": "scattered clouds",
+        "avgHumidity": 65.875,
+        "avgPressure": 1020.625
+    },
+    {
+        "date": "2024-06-20",
+        "avgTemp": 16.43375,
+        "description": "overcast clouds",
+        "avgHumidity": 59.875,
+        "avgPressure": 1020.5
+    },
+    {
+        "date": "2024-06-21",
+        "avgTemp": 15.603750000000002,
+        "description": "overcast clouds",
+        "avgHumidity": 73.125,
+        "avgPressure": 1010.5
+    },
+    {
+        "date": "2024-06-22",
+        "avgTemp": 15.16375,
+        "description": "overcast clouds",
+        "avgHumidity": 84.75,
+        "avgPressure": 1010.125
+    },
+    {
+        "date": "2024-06-23",
+        "avgTemp": 17.05857142857143,
+        "description": "overcast clouds",
+        "avgHumidity": 72.42857142857143,
+        "avgPressure": 1017.8571428571429
+    }
+],
+    "status": 200,
+    "statusText": "OK",
+    "headers": {"content-length": "472", "content-type": "application/json; charset=utf-8"},
+    "config": {
+        "transitional": {"silentJSONParsing": true, "forcedJSONParsing": true, "clarifyTimeoutError": false},
+        "adapter": ["xhr", "http", "fetch"],
+        "transformRequest": [null],
+        "transformResponse": [null],
+        "timeout": 0,
+        "xsrfCookieName": "XSRF-TOKEN",
+        "xsrfHeaderName": "X-XSRF-TOKEN",
+        "maxContentLength": -1,
+        "maxBodyLength": -1,
+        "env": {},
+        "headers": {"Accept": "application/json, text/plain, */*"},
+        "method": "get",
+        "url": "http://localhost:3000/weather?location=London"
+    },
+    "request": {}
+    }`;
             const location = req.query.location;
             const response = await WeatherApiService.getWeather(location);
-            res.status(200).json(response);
+            res.status(200).json(response); // JSON.parse(hardcodedResponse)
         } catch (error) {
             res.status(500).json({ error: error.message });
         }
--- travel-info-server/src/services/WeatherApi.service.js
+++ travel-info-server/src/services/WeatherApi.service.js
@@ -1,10 +1,49 @@
 import axios from "axios";
 
 export default class WeatherApiService {
-    static async getWeather(location) {
+    static getWeather = async (location) => {
         const response = await axios.get(
-            `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=38fb2dd35fee522d8ec4f4fb7793f9df&units=metric`
+            `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=38fb2dd35fee522d8ec4f4fb7793f9df&units=metric`
         );
-        return response.data;
+        return this.#processWeatherData(response.data);
+    };
+
+    //* The /forecast API returns data in 3-hour intervals for the next 5 days.
+    //* This function processes the data to return an array of 5 entries, with daily averages, by reducing and accumulating the data
+    static #processWeatherData(data) {
+        //? This is an array with a date as key, and for value: an object with [temperature, description, humidity, pressure] arrays with the values for each day
+        const processedData = data.list.reduce((accumulator, current) => {
+            const date = current.dt_txt.split(" ")[0]; // Extract the date in YYYY-MM-DD format
+            // Create entry for each day if it doesn't exist
+            if (!accumulator[date]) {
+                accumulator[date] = {
+                    temps: [],
+                    descriptions: [],
+                    humidity: [],
+                    pressure: [],
+                };
+            }
+            // Push the temperature, description, humidity, and pressure values to the corresponding arrays
+            accumulator[date].temps.push(current.main.temp);
+            accumulator[date].descriptions.push(current.weather[0].description);
+            accumulator[date].humidity.push(current.main.humidity);
+            accumulator[date].pressure.push(current.main.pressure);
+            return accumulator;
+        }, {});
+
+        // Reduce the data into a single array of objects containing the averages for each parameter, each day
+        return Object.keys(processedData).map((date) => ({
+            date,
+            avgTemp:
+                processedData[date].temps.reduce((a, b) => a + b, 0) /
+                processedData[date].temps.length,
+            description: processedData[date].descriptions[0], //? Taking the first description of the day for simplicity
+            avgHumidity:
+                processedData[date].humidity.reduce((a, b) => a + b, 0) /
+                processedData[date].humidity.length,
+            avgPressure:
+                processedData[date].pressure.reduce((a, b) => a + b, 0) /
+                processedData[date].pressure.length,
+        }));
     }
 }
